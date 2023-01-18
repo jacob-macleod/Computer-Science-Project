69062d63a628bc1a8085952680276e3713ac943c
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-01-18 10:25:29 +0000

    Removed some functions in dashboard.html to it's own file

---
+++ static/dashboard.js
@@ -0,0 +1,90 @@
+alert (document.getElementById("selectedMenuItem").innerHTML + " seperate file")
+// Set up the functionality for the hamburger menu
+hamburgerMenu();
+// Set up the menu bar of the page
+isAdmin = "{{ isAdmin }}";
+setUpMenuBar(isAdmin, "{{ firstName }}");
+// If the user is an admin, show pages that are restricted by default
+showRestrictedPages(isAdmin);
+
+// Find current day
+today = new Date();
+// Find current year
+year = today.getFullYear();
+// Find the current month, then add a 0 at the start if teh date is less than 10. So 7 turns to 07
+month = parseInt(today.getMonth()) + 1;
+if (month < 10) {
+    month = "0" + month.toString();
+} else {
+    month = month.toString();
+}
+
+// Do the same as we did for the month for the day
+day = parseInt(today.getDate());
+if (day < 10) {
+    day = "0" + day.toString();
+} else {
+    day = day + day.toString();
+}
+
+date = year + "-" + month + "-" + day
+
+// Make the date selector show the date for today
+document.getElementById("dateSelector").setAttribute("value", date);
+
+function changeDaySelected() {
+    day = document.getElementById("dateSelector").value
+    formattedDay = day.split("-")[2] + "/" + day.split("-")[1] + "/" + day.split("-")[0]
+    datalogger = document.getElementById("dataloggerSelector").value
+    window.open("/getDay?day=" + formattedDay + "&datalogger=" + datalogger, "_self")
+}
+
+
+// Fill the datalogger select element with values
+dataloggers = "{{ dataloggers }}";
+dataloggersArr = dataloggers.split(",");
+for (i=0;i<dataloggersArr.length;i++) {
+        if (dataloggersArr[i] != "") {
+        option = document.createElement("option");
+        option.value = dataloggersArr[i];
+        option.innerHTML = dataloggersArr[i];
+        document.getElementById("dataloggerSelector").appendChild(option);
+    }
+}
+
+data = "{{ data }}";
+if (data != "") {
+    labels = "{{ labels }}";
+    dataArr = data.split("*");
+    console.log(data)
+    console.log(dataArr);
+    date = dataArr.pop();
+    labelsArr = labels.split(",");
+
+    // Date is in dd/mm/yyy format. Change it to yyy-mm-dd
+    year = date.split("/")[2];
+    month = date.split("/")[1];
+    day = date.split("/")[0];
+    normalisedDate = year + "-" + month + "-" + day;
+
+    document.getElementById("dateSelector").value = normalisedDate;
+
+    // Turn data into seperate arrays of humidity, light levels, etc
+    var lightLevels = dataArr[1].split(",");
+    var humidity = dataArr[3].split(",");
+    var temperature = dataArr[5].split(",");
+    var phLevel = dataArr[7].split(",");
+    var soilMoisture = dataArr[9].split(",");
+
+
+    drawChart(document.getElementById("lightLevels"), labelsArr, lightLevels, "Light Levels", "#96F3CC", "#BEBBF7");
+    drawChart(document.getElementById("humidity"), labelsArr, humidity, "Humidity", "#BEBBF7", "#4EA699");
+    drawChart(document.getElementById("temperature"), labelsArr, temperature, "Temperature", "#7772B1", "#A6CEE3");
+    drawChart(document.getElementById("phLevel"), labelsArr, phLevel, "PH Level", "#BEBBF7", "#A6CEE3");
+    drawChart(document.getElementById("soilMoisture"), labelsArr, soilMoisture, "Soil Moisture", "#7772B1", "#A6CEE3");
+} else {
+    // Hide the graph
+    document.getElementById("graphsRow1").style.display = "none";
+    document.getElementById("graphsRow2").style.display = "none";   
+    document.getElementById("feedbackText").innerHTML = "Sorry, no data was found for the selected day. Consider changing the day selected to view more data"
+}
---
+++ static/sharedFunctions.js
@@ -0,0 +1,123 @@
+/*
+    Contains common functions used by multiuple files
+*/
+
+// Provides the functionality for the hamburger menu, if it is visible
+// When the user clicks the hamburger icon, change the icon and show the hamburger menu. When the user clicks it again, do the opposite
+function hamburgerMenu () {
+    // Get the icon for the hamburger menu
+    icon = document.getElementById("hamburgerIcon");
+
+    // When the icon is clicked
+    icon.addEventListener("click", () => {
+        // Switch the image that it displays to the image
+        // Either "Hamburger.svg" or "close.svg" is shown
+        if (icon.getAttribute("src") == "static/Hamburger.svg") {
+	        icon.setAttribute("src", "static/close.svg");
+            document.getElementById("menuItems").style.display = "grid";
+        } else {
+            icon.setAttribute("src", "static/Hamburger.svg")
+            document.getElementById("menuItems").setAttribute("style", "display:none;")
+        }
+    });
+}
+
+// Delete all cookies
+function deleteCookies() {
+    // Get all the cookies
+    var cookies = document.cookie.split(";");
+
+    // For every cookie, edit it to make it expire
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var value = cookie.indexOf("=");
+        var name = value > -1 ? cookie.substr(0, value) : cookie;
+        // To delete it, make it so it's already expired
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+// Logout the user
+function logout() {
+    // Delete all cookies and load the / page. Since there are no cookies the sign in page will load
+    deleteCookies();
+    window.open("/", "_self");
+}
+
+// Get the value of a cookie by it's name
+function getCookie(cookieName) {
+    singleCookie = decodeURIComponent(document.cookie)
+    singleCookie = singleCookie.toString();
+    singleCookie = singleCookie.split(";");
+
+    // Look at every cookie
+    for (i=0;i<singleCookie.length;i++) {
+        // If the cookie's name is right
+        if (singleCookie[i].split("=")[0] == cookieName) {
+            // Return the cookie's value
+            return singleCookie[i].split("=")[1];
+        }
+    }    
+}
+
+/* 
+    Set up the menu bar of the page. This includes:
+    * Making the admin indicator work
+    * Making the admin indicator blue if the user is an admin
+    * Setting the username and farm name
+*/
+function setUpMenuBar (isAdmin, firstName) {
+    // Show the correct text on the admin indicator
+    document.cookie = "isAdmin={{ isAdmin }}";
+
+    // Set up farm name and username
+    document.getElementById("firstName").innerHTML = firstName
+    document.getElementsByName("username")[0].innerHTML = getCookie("username");
+
+    // Make the admin indicator blue if the user is an owner
+    if (isAdmin == "True") {
+        document.getElementsByName("adminIndicator")[0].innerHTML = "Owner";
+        document.getElementsByName("adminIndicator")[0].style = "display: inline-block; color: #A6CEE3;";
+    }
+}
+
+function showRestrictedPages (isAdmin) {
+    // Some pages are hidden by default. If the user is an admin, show them
+    if (isAdmin == "True") {
+        // Get all the items to hide
+        itemsToHide = document.getElementsByName("restrictedItem");
+        for (i=0;i<itemsToHide.length;i++) {
+            // Change their visibility to hidden
+            itemsToHide[i].style = "display: inline-block; visibility:visible;";
+        }
+    }
+}
+
+// Set up a chart
+function drawChart(elementToDrawOn, labels, data, name, color1, color2) {
+    // Set up the light level chart
+    const ctx = elementToDrawOn;
+        
+    // Create gradient
+    let gradient = ctx.getContext('2d').createLinearGradient(0, 0, 400, 0);
+    gradient.addColorStop(0, color1);
+    gradient.addColorStop(1, color2);
+
+    // Create chart
+    new Chart(ctx, {
+    type: 'line',
+    data: {
+    labels: labels,
+    datasets: [{
+        label: name,
+        data: data,
+        fill: false,
+        borderColor: gradient,
+        tension: 0.05
+    }]
+    },
+    options:  {
+        maintainAspectRatio:false
+    }
+    });
+    }
