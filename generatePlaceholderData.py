efd987ee89c92ec8cd876428880369ef395d049e
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-01-20 14:34:04 +0000

    Reverted code to generate placeholder data back to older more random version

--- generatePlaceholderData.py
+++ generatePlaceholderData.py
@@ -13,7 +13,7 @@ def generatePlaceholderData(numberOfValues) :
     # f(x)=RandomBetween(1,5) sin(RandomBetween(1,3) x)+((RandomBetween(-5,5))/(10)) x
     for i in range(0, numberOfValues):
         x = i/10
-        itemToAppend= str((math.sin(randomNumber2*x)) + (x*((randomNumber3)/10)))
+        itemToAppend= str(int(random.randint(1,5) * (random.randint(1,5)*x)) + (x*((random.randint(-5,5))/10)))
         dataItems.append(itemToAppend)
     
 
