b6e8f625dfa3a52ce5d52bb6532cffd62fc19f09
jacob-macleod <jacob.e.macleod@gmail.com>
2023-01-29 14:35:40 +0000

    Added a show/hide password button to the sign in page

--- static/js/signIn.js
+++ static/js/signIn.js
@@ -1,3 +1,18 @@
 function togglePasswordVisibility () {
-    alert ("clicked");
+    // Get some variables
+    var passwordBox = document.getElementsByName("password")[0];
+    passwordBoxType = passwordBox.getAttribute("type");
+    button = document.getElementsByName("togglePassword")[0];
+
+    // If the password is hidden
+    if (passwordBoxType == "password") {
+        // Then show the password
+        passwordBox.setAttribute("type", "");
+        button.innerHTML = "Hide Password"
+    } else { // Else if the password is not hidden
+        // Then hide the password
+        passwordBox.setAttribute("type", "password");
+        button.innerHTML = "Show Password"
+    }
+    event.preventDefault();
 }
