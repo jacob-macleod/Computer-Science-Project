8c31b3b6214ac724727ac67c901bde5a27eb3d79
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-02-09 12:55:28 +0000

    Removed an alert in javascript which was present for debugging

--- static/js/addWorker.js
+++ static/js/addWorker.js
@@ -4,9 +4,7 @@ function addWorker() {
     username = document.getElementsByName("username")[1].value;
     password = document.getElementById("passwordInputBox").value;
     passwordConfirmation = document.getElementById("passwordConfirmationInputBox").value;
-    //console.log (document.getElementsByName("firstName")[0].value);
 
     pageToLoad = "/make-worker?firstName=" + firstName + "&lastName=" + lastName + "&username=" + username + "&password=" + password + "&passwordConfirmation=" + passwordConfirmation;
-    alert (pageToLoad);
     window.open(pageToLoad, "_self");
 }
