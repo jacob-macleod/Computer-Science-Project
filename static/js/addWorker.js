bd3a83ada3770c7ae263e39282ada36f1d25d7bb
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-02-10 15:57:46 +0000

    Made the make-worker page store details typed in if the page reloads

--- static/js/addWorker.js
+++ static/js/addWorker.js
@@ -8,3 +8,39 @@ function addWorker() {
     pageToLoad = "/make-worker?firstName=" + firstName + "&lastName=" + lastName + "&username=" + username + "&password=" + password + "&passwordConfirmation=" + passwordConfirmation;
     window.open(pageToLoad, "_self");
 }
+
+//Select all input boxes
+inputBoxes = document.querySelectorAll('input');
+
+// Set the value of the input boxes to equal the equivalent text in the cookie, if the associated feedback text is invisible 
+if (getCookie(" workerInputBoxText") == undefined) {
+    document.cookie = " workerInputBoxText=,,,,,,";
+} else {
+    inputBoxText = getCookie(" workerInputBoxText").split(",");
+    feedbackTexts = document.getElementsByName("feedbackText");
+
+    for (i=0;i<inputBoxes.length;i++) {
+        console.log(i);
+        if (feedbackTexts[i].innerHTML == "") {
+            inputBoxes[i].value = inputBoxText[i];
+        }
+     }
+}
+
+// When an input box is clicked
+function onInputBoxClick () {
+    inputBoxText = "";
+
+    // Append the text in all the input boxes to a string
+    for (i=0;i<inputBoxes.length;i++) {
+        inputBoxText = inputBoxText + inputBoxes[i].value + ","
+    }
+
+    // And save that string as the cookie
+     document.cookie = "workerInputBoxText=" + inputBoxText;
+}
+
+// For every input box add an event listener
+for (i=0;i<inputBoxes.length;i++) {
+    inputBoxes[i].addEventListener("keyup", () => {onInputBoxClick()});
+}
