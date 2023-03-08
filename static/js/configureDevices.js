00d2519a3b21b2d6ff57e1bc5482a78b109f870a
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-03-08 10:03:49 +0000

    Solved a bug where there was no error displayed when no dataloggers where added

--- static/js/configureDevices.js
+++ static/js/configureDevices.js
@@ -6,32 +6,38 @@ setUpMenuBar(isAdmin);
 showRestrictedPages(isAdmin);
 
 // Add an element to the table for each of the dataloggers
-for (i=0;i<dataloggers.length;i++) {
-    if (dataloggers[i] != "") {
-        // Create the container to hold the datalogger name and remove button
-        container = document.createElement("d");
-        container.id = "tableItem";
+if (dataloggers.length != 1) {
+    for (i=0;i<dataloggers.length;i++) {
+        if (dataloggers[i] != "") {
+            // Create the container to hold the datalogger name and remove button
+            container = document.createElement("d");
+            container.id = "tableItem";
 
-        // Create the paragraph which holds the datalogger name
-        deviceName = document.createElement("p");
-        deviceName.innerHTML = dataloggers[i];
-        deviceName.id = "itemName";
+            // Create the paragraph which holds the datalogger name
+            deviceName = document.createElement("p");
+            deviceName.innerHTML = dataloggers[i];
+            deviceName.id = "itemName";
 
-        // Create the button used to remove the datalogger
-        button = document.createElement("button");
-        button.innerHTML = "Remove";
-        button.setAttribute("onclick", "removeDatalogger()");
-        button.setAttribute("name", dataloggers[i]);
-        button.id = "outlinedButton";
+            // Create the button used to remove the datalogger
+            button = document.createElement("button");
+            button.innerHTML = "Remove";
+            button.setAttribute("onclick", "removeDatalogger()");
+            button.setAttribute("name", dataloggers[i]);
+            button.id = "outlinedButton";
 
-        // Append deviceName and button to the container
-        container.appendChild(deviceName);
-        container.appendChild(button);
+            // Append deviceName and button to the container
+            container.appendChild(deviceName);
+            container.appendChild(button);
 
-        // Append the container to the table
-        document.getElementById("table").appendChild(container);
+            // Append the container to the table
+            document.getElementById("table").appendChild(container);
+        }
     }
+} else {
+    document.getElementById("feedbackText").innerHTML = "Sorry, you haven't added any dataloggers yet";
+    document.getElementById("table").style.display = "none";
 }
+
 
 function removeDatalogger() {
     deviceName = event.target.name;
